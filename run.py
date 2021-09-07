d45b39826e0cf358eec6b9f1150e8174deda9956
ItoArbi <90256587+ItoArbi@users.noreply.github.com>
2021-09-07 19:12:51 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+from app import application
+# Jika ada kode di dalam blok if __name__ == "__main__" selain app.run(),
+# maka masukkan di sini.
+# Misalnya kamu punya fungsi lain yang didekorasi oleh Flask(__name__).before_first_request
+# maka tambahkan di sini
